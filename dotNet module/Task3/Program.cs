445520ba854e9f285dd1bd41e7ff99bacb1bf39e
galexal <galexal@yandex.ru>
2021-03-03 19:52:25 +0400

    Сделал третье задание.

--- dotNet module/Task3/Program.cs
+++ dotNet module/Task3/Program.cs
@@ -2,18 +2,27 @@
 
 namespace Task3
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             var circle = new Circle(5);
             Console.WriteLine(circle.Length);
             var round = new Round(5);
-            Console.WriteLine(round.Square());
+            Console.WriteLine(round.Area());
             Console.WriteLine(round.Length);
             var ring = new Ring(5, 3);
-            Console.WriteLine(ring.Square());
+            Console.WriteLine(ring.Area());
+            var square = new Square(5);
+            Console.WriteLine(square.Length());
+            Console.WriteLine(square.Area());
+            var rectangle = new Rectangle(5, 4);
+            Console.WriteLine(rectangle.Length());
+            Console.WriteLine(rectangle.Area());
+            var triangle = new Triangle(5, 4, 3);
+            Console.WriteLine(triangle.Length());
+            Console.WriteLine(triangle.Area());
         }
     }
 }
--- dotNet module/Task3/Ring.cs
+++ dotNet module/Task3/Ring.cs
@@ -1,19 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Task3
 {
     public class Ring : Round
     {
         public int InnerRadius { get; set; }
+
         public Ring() { }
+
         public Ring(int outerRadius, int innerRadius)
         {
             this.OuterRadius = outerRadius;
             this.InnerRadius = innerRadius;
         }
-        public override double Square() => Math.PI * this.OuterRadius * this.OuterRadius
-        - Math.PI * this.InnerRadius * this.InnerRadius;
+
+        public override double Area() => (Math.PI * this.OuterRadius * this.OuterRadius)
+        - (Math.PI * this.InnerRadius * this.InnerRadius);
     }
 }
