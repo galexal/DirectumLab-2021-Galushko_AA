760bf5e43f879942c7ee284e9995be89f2bed928
galexal <galexal@yandex.ru>
2021-03-03 19:52:25 +0400

    Сделал третье задание.

--- dotNet module/Task3/Circle.cs
+++ dotNet module/Task3/Circle.cs
@@ -7,8 +7,11 @@ namespace Task3
     public class Circle : Shape
     {
         public int OuterRadius { get; set; }
+
         public double Length => 2 * Math.PI * this.OuterRadius;
+
         public Circle() { }
+
         public Circle(int outerRadius)
         {
             this.OuterRadius = outerRadius;
