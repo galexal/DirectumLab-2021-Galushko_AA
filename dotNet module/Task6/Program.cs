c76420cbbebccff0cba5ade08f35318a3c675df4
galexal <galexal@yandex.ru>
2021-03-09 20:27:35 +0500

    Сделано задание 6.

--- dotNet module/Task6/Program.cs
+++ dotNet module/Task6/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Task6
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static int Counter(string path, DateTime startDate, DateTime endDate)
         {
-            Console.WriteLine("Hello World!");
+            string data;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                data = sr.ReadToEnd();
+            }
+            Regex regex = new Regex(@"\d{1,2}.\d{1,2}.\d{4}");
+            var count = 0;
+
+            MatchCollection matches = regex.Matches(data);
+            foreach (Match match in matches)
+                if (DateTime.Parse(match.Value) >= startDate
+                    && DateTime.Parse(match.Value) <= endDate)
+                    count++;
+
+            return count;
+        }
+        
+        public static void Main(string[] args)
+        {
+            var path = "ClientConnectionLog.log";
+            var startDate = new DateTime(2007, 12, 19);
+            var endDate = new DateTime(2008, 01, 22);
+
+            Console.WriteLine($"Найдено {Counter(path,startDate,endDate)} совпадений");
         }
     }
 }
