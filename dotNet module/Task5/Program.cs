e43bde31c72fcbbf20a317fff52036cc00a8a30c
galexal <galexal@yandex.ru>
2021-03-09 15:47:57 +0500

    Сделал задания 5-1, 5-2.

---
+++ dotNet module/Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(new StringValue("AAA").Equals(new StringValue("AAA")));
+            Console.WriteLine(new StringValue("AAA") == new StringValue("AAA"));
+        }
+    }
+}
---
+++ dotNet module/Task5/StringValue.cs
@@ -0,0 +1,35 @@
+﻿namespace Task5
+{
+    public class StringValue
+    {
+        public string Value { get; private set; }
+
+        public StringValue(string value)
+        {
+            this.Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType()) return false;
+
+            StringValue stringValue = (StringValue)obj;
+            return this.Value == stringValue.Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Value.GetHashCode();
+        }
+
+        public static bool operator ==(StringValue sv1, StringValue sv2)
+        {
+            return sv1.Value == sv2.Value;
+        }
+
+        public static bool operator !=(StringValue sv1, StringValue sv2)
+        {
+            return sv1.Value != sv2.Value;
+        }
+    }
+}
