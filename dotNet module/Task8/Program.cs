c4293a0f7634822252f30ffefc5973085c41ba46
galexal <galexal@yandex.ru>
2021-03-11 15:48:32 +0500

    Сделал задание 8-2.

---
+++ dotNet module/Task8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(1, 2, 3));
+            Console.WriteLine(GetMax("A", "BC", "CDE"));
+        }
+
+        public static T GetMax<T>(T firstValue, T secondValue, T thirdValue) where T : IComparable
+        {
+            if (firstValue.CompareTo(secondValue) < 0)
+            {
+                if (secondValue.CompareTo(thirdValue) < 0) return thirdValue;
+                else return secondValue;
+            }
+            else
+            {
+                if (firstValue.CompareTo(thirdValue) < 0) return thirdValue;
+                else return firstValue;
+            }
+        }
+    }
+}
