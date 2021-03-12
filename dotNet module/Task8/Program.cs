7d5ce1fb1cb4e6afe627e48fcc9caf304c8aac12
galexal <galexal@yandex.ru>
2021-03-12 18:03:37 +0500

    Почему-то не все изменения запушились. 8-1 выполнено в Task7.Form1.Designer

--- dotNet module/Task8/Program.cs
+++ dotNet module/Task8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task8
 {
@@ -8,6 +9,31 @@ namespace Task8
         {
             Console.WriteLine(GetMax(1, 2, 3));
             Console.WriteLine(GetMax("A", "BC", "CDE"));
+            Console.WriteLine();
+
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            var dictionary = new Dictionary<string, string>()
+            {
+                ["red"] = "красный",
+                ["green"] = "зеленый",
+                ["yellow"] = "желтый"
+            };
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} переводится как {item.Value}");
+            }
+
+            var lines = new LinesIterator("ClientConnectionLog.log");
+            foreach (var item in lines)
+            {
+                Console.WriteLine(item);
+            }
         }
 
         public static T GetMax<T>(T firstValue, T secondValue, T thirdValue) where T : IComparable
