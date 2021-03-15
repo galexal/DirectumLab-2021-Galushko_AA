438ba4f7372946af74b0522bcf2aed3e5e0e26c8
galexal <galexal@yandex.ru>
2021-03-15 15:18:41 +0500

    Сделал задание 10

---
+++ dotNet module/Task10/FastSearcher.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    public class FastSearcher
+    {
+        public List<int> Data { get; set; }
+
+        public List<int> Result { get; set; } = new List<int>();
+
+        public delegate bool SearchCondition(int x);
+
+        public int MaxTasks { get; set; } = 1;
+
+        public int MinNumberOfValues { get; set; } = 10;
+
+        public FastSearcher(List<int> data, int maxTasks = 1, int minNumberOfValues = 10)
+        {
+            this.Data = data;
+            this.MaxTasks = maxTasks;
+            this.MinNumberOfValues = minNumberOfValues;
+        }
+
+        public void TaskMaker(SearchCondition sc)
+        {
+            var tasks = new Task[this.MaxTasks];
+            for (int i = 0; i < this.MaxTasks; i++)
+            {
+                Task task = new Task(() => this.Find(sc));
+                tasks[i] = task;
+                task.Start();
+                if (this.Data.Count <= this.MinNumberOfValues) break;
+            }
+            if (tasks != null) Task.WaitAll(tasks);
+        }
+
+        public void Find(SearchCondition sc)
+        {
+            for (int i = 0; i < this.Data.Count; i++)
+            {
+                if (sc(this.Data[i]))
+                {
+                    this.Result.Add(this.Data[i]);
+                }
+            }
+        }
+    }
+}
---
+++ dotNet module/Task10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task10
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var rnd = new Random();
+            var list = new List<int>();
+            var listLength = 100;
+            for (int i = 0; i < listLength; i++)
+            {
+                list.Add(rnd.Next(listLength));
+            }
+
+            var fs = new FastSearcher(list, 2, 20);
+
+            fs.TaskMaker(x => x < 10);
+
+            for (int i = 0; i < fs.Result.Count; i++)
+            {
+                Console.WriteLine(fs.Result[i]);
+            }
+        }
+    }
+}
