886d92a35b33eb02c384b8a32218871aaebd6496
galexal <galexal@yandex.ru>
2021-03-23 23:12:56 +0500

    Практика 13

---
+++ dotNet module/Task13/EarlyBinding.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Task13
+{
+    /// <summary>
+    /// Раннее связывание.
+    /// </summary>
+    public class EarlyBinding
+    {
+        /// <summary>
+        /// Запись в Excel таблицы умножения.
+        /// </summary>
+        public static void Main()
+        {
+            var excelapp = new Excel.Application();
+            excelapp.Visible = true;
+            excelapp.SheetsInNewWorkbook = 1;
+            excelapp.Workbooks.Add(Type.Missing);
+            var excelappworkbooks = excelapp.Workbooks;
+            var excelappworkbook = excelappworkbooks[1];
+            var excelsheets = excelappworkbook.Worksheets;
+            var excelworksheet = (Excel.Worksheet)excelsheets.get_Item(1);
+            for (var m = 1; m < 9; m++)
+            {
+                for (var n = 1; n < 9; n++)
+                {
+                    var excelcells = (Excel.Range)excelworksheet.Cells[m, n];
+                    excelcells.Value2 = m * n;
+                }
+            }
+        }
+    }
+}
---
+++ dotNet module/Task13/LateBinding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace Task13
+{
+    /// <summary>
+    /// Позднее связывание.
+    /// </summary>
+    public class LateBinding
+    {
+        /// <summary>
+        /// Запись в Excel таблицы умножения.
+        /// </summary>
+        public static void Main()
+        {
+            dynamic excel = Activator.CreateInstance(Type.GetTypeFromProgID(
+              "Excel.Application"));
+            excel.Visible = true;
+            var workbooks = excel.GetType().InvokeMember("Workbooks", BindingFlags.GetProperty, null, excel, null);
+            var workbook = workbooks.GetType().InvokeMember("Add", BindingFlags.InvokeMethod, null, workbooks, null);
+            var worksheets = workbook.GetType().InvokeMember("Worksheets", BindingFlags.GetProperty, null, workbook, null);
+            var args = new object[1];
+            args[0] = 1;
+            var worksheet = worksheets.GetType().InvokeMember("Item", BindingFlags.GetProperty, null, worksheets, args);
+            for (var m = 1; m < 9; m++)
+            {
+                for (var n = 1; n < 9; n++)
+                {
+                    // Не могу разобраться, почему все значения в первую строку записываются
+                    var range = worksheet.GetType().InvokeMember("Cells", BindingFlags.GetProperty, null, worksheet, new object[] { $"{m},{n}" });
+                    range.GetType().InvokeMember("Value", BindingFlags.SetProperty, null, range, new object[] { $"{m * n}" });
+                }
+            }
+        }
+    }
+}
