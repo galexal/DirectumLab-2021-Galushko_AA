c8a146a3714acde817c92befa42fc2500e258ab1
galexal <galexal@yandex.ru>
2021-03-24 17:21:35 +0500

    Сделано задание 14.

---
+++ dotNet module/LocalizedApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LocalizedApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var resMan = new ResourceManager("LocalizedApp.TextMessages",
+              Assembly.GetExecutingAssembly());
+            MessageBox.Show(resMan.GetString("HelloMessage",
+              new System.Globalization.CultureInfo("en")));
+
+
+        }
+    }
+}
