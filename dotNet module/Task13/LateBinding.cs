9c276ea59bf89781fe16f20d2f8af894a692c59d
galexal <galexal@yandex.ru>
2021-03-24 11:36:51 +0500

    Исправил замечания.

--- dotNet module/Task13/LateBinding.cs
+++ dotNet module/Task13/LateBinding.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Reflection;
 
 namespace Task13
 {
@@ -22,32 +21,15 @@ namespace Task13
             var excelappworkbooks = excelapp.Workbooks;
             var excelappworkbook = excelappworkbooks[1];
             var excelsheets = excelappworkbook.Worksheets;
-            var excelworksheet = /*(Excel.Worksheet)*/excelsheets.get_Item(1);
+            var excelworksheet = excelsheets[1];
             for (var m = 1; m < 9; m++)
             {
                 for (var n = 1; n < 9; n++)
                 {
-                    var excelcells = /*(Excel.Range)*/excelworksheet.Cells[m, n];
+                    var excelcells = excelworksheet.Cells[m, n];
                     excelcells.Value2 = m * n;
                 }
             }
-
-            //excel.Visible = true;
-            //var workbooks = excel.GetType().InvokeMember("Workbooks", BindingFlags.GetProperty, null, excel, null);
-            //var workbook = workbooks.GetType().InvokeMember("Add", BindingFlags.InvokeMethod, null, workbooks, null);
-            //var worksheets = workbook.GetType().InvokeMember("Worksheets", BindingFlags.GetProperty, null, workbook, null);
-            //var args = new object[1];
-            //args[0] = 1;
-            //var worksheet = worksheets.GetType().InvokeMember("Item", BindingFlags.GetProperty, null, worksheets, args);
-            //for (var m = 1; m < 9; m++)
-            //{
-            //    for (var n = 1; n < 9; n++)
-            //    {
-            //        // Не могу разобраться, почему все значения в первую строку записываются
-            //        var range = worksheet.GetType().InvokeMember("Cells", BindingFlags.GetProperty, null, worksheet, new object[] { $"{m},{n}" });
-            //        range.GetType().InvokeMember("Value", BindingFlags.SetProperty, null, range, new object[] { $"{m * n}" });
-            //    }
-            //}
         }
     }
 }
