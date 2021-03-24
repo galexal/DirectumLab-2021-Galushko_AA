669c46d6a8fece173d67a1fd3a29e213071203d1
galexal <galexal@yandex.ru>
2021-03-24 12:49:19 +0500

    Сделал задание 12-4.

---
+++ dotNet module/Task12/ConfigReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+
+namespace Task12
+{
+    /// <summary>
+    /// Чтение настроек файла конфигурации.
+    /// </summary>
+    public class ConfigReader
+    {
+        /// <summary>
+        /// Чтение настроек файла конфигурации.
+        /// </summary>
+        public static void PrintSettings()
+        {
+            var settings = ConfigurationManager.AppSettings;
+            foreach (var key in settings.AllKeys)
+            {
+                Console.WriteLine($"Имя: {key}\nЗначение: {settings[key]}");
+            }
+        }
+    }
+}
