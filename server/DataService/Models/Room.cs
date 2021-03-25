0bae462949e0ade349bb16a5a0d1155ddb4f4989
galexal <galexal@yandex.ru>
2021-03-25 16:00:39 +0500

    PlanPokerBranch1

---
+++ server/DataService/Models/Room.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataService.Models
+{
+    /// <summary>
+    /// Комната.
+    /// </summary>
+    public class Room : IEntity
+    {
+        /// <summary>
+        /// Id.
+        /// </summary>
+        public Guid Id { get; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Хозяин комнаты.
+        /// </summary>
+        public Guid OwnerId { get; set; }
+
+        /// <summary>
+        /// Участники.
+        /// </summary>
+        public List<Guid> Participants { get; set; }
+
+        /// <summary>
+        /// Обсуждения.
+        /// </summary>
+        public List<Guid> Discussions { get; set; }
+
+        /// <summary>
+        /// Конструктор комнаты.
+        /// </summary>
+        /// <param name="name">Имя комнаты.</param>
+        /// <param name="ownerId">Хозяин комнаты.</param>
+        public Room(string name, Guid ownerId)
+        {
+            this.Name = name;
+            this.OwnerId = ownerId;
+        }
+    }
+}
---
+++ server/PlanPoker/Services/RoomServices.cs
@@ -0,0 +1,60 @@
+﻿using DataService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanPoker.Services
+{
+    /// <summary>
+    /// Сервисы комнаты.
+    /// </summary>
+    public class RoomServices
+    {
+        /// <summary>
+        /// Состояние текущей комнаты.
+        /// </summary>
+        public Room CurrentRoom { get; set; }
+
+        /// <summary>
+        /// Создаnть комнату.
+        /// </summary>
+        /// <param name="name">Имя комнаты.</param>
+        /// <param name="ownerId">Хозяин комнаты.</param>
+        /// <returns>Новая комната.</returns>
+        public void Create(string name, Guid ownerId)
+        {
+            this.CurrentRoom = new Room(name, ownerId);
+        }
+
+        /// <summary>
+        /// Добавить участника.
+        /// </summary>
+        /// <param name="user">ИД участника.</param>
+        public void AddUser(Guid userId)
+        {
+            if (!this.CurrentRoom.Participants.Contains(userId))
+                this.CurrentRoom.Participants.Append(userId);
+        }
+
+        /// <summary>
+        /// Удалить участника.
+        /// </summary>
+        /// <param name="user">ИД участника.</param>
+        public void RemoveUser(Guid userId)
+        {
+            if(this.CurrentRoom.Participants.Contains(userId))
+                this.CurrentRoom.Participants.Remove(userId);
+        }
+
+        /// <summary>
+        /// Изменить хозяина комнаты.
+        /// </summary>
+        /// <param name="user">ИД участника.</param>
+        public void ChangeOwner(Guid userId)
+        {
+            if (this.CurrentRoom.Participants.Contains(userId))
+                this.CurrentRoom.OwnerId = userId;
+        }
+    }
+}
