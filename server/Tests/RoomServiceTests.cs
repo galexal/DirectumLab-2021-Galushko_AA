df6877d07be6d02cd2ab6c49a3bffac20eba6f4e
galexal <galexal@yandex.ru>
2021-04-05 13:57:45 +0500

    Добавлены тесты.

---
+++ server/Tests/RoomServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using DataService.Repositories;
+using NUnit.Framework;
+using PlanPoker.Services;
+using System;
+
+namespace Tests
+{
+    public class RoomServiceTests
+    {
+        private UserRepository userRepository;
+        private UserService userService;
+        private RoomRepository roomRepository;
+        private RoomService roomService;
+
+        [OneTimeSetUp]
+        public void CreateServiceAndRepository()
+        {
+            this.userRepository = new UserRepository();
+            this.userService = new UserService(this.userRepository);
+            this.roomRepository = new RoomRepository();
+            this.roomService = new RoomService(this.roomRepository, this.userRepository);
+        }
+
+        [Test]
+        [TestCase("RoomName", "fb7b906e-4191-4e08-98b2-f65ca94cba05")]
+        public void CreateRoom(string name, Guid ownerId)
+        {
+            var room = this.roomService.Create(name, ownerId);
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(name, room.Name);
+                Assert.AreEqual(ownerId, room.OwnerId);
+            });
+        }
+
+        [Test]
+        public void AddUserToRoom()
+        {
+            var roomOwner = this.userService.Create("OwnerName");
+            var room = this.roomService.Create("RoomName", roomOwner.Id);
+            var newUser = this.userService.Create("NewUserName");
+            this.roomService.AddUser(newUser.Id, room.Id, roomOwner.Token);
+            Assert.AreEqual(true, room.Participants.Contains(newUser.Id));
+        }
+    }
+}
